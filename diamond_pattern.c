b66b5497084df01f80b0b46afdcd379588affd18
yashraj8703 <rajyash8703@gmail.com>
2025-01-02 21:02:59 +0530

    diamond_pattern

---
+++ diamond_pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+	int row;
+	int col;
+	for(row=0;row<n;row++)
+	{
+		for(col=0;col<n-row-1;col++)
+		{
+			printf(" ");
+		}
+		for(col=0;col<2*row+1;col++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	row=0;
+	col=0;
+	for(row=0;row<n;row++)
+	{
+		for(col=0;col<row;col++)
+		{
+			printf(" ");
+		}
+		for(col=0;col<2*(n-(row+1))+1;col++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+    return 0;
+}
